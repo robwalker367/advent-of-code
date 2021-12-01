1c26e1f6f26005718be8b9e39fae45ac50155810
Rob Walker <robertsrwalker@gmail.com>
2021-12-01 00:20:28 -0500

    Add part1

--- 2021/day1/day1.cc
+++ 2021/day1/day1.cc
@@ -1,6 +1,25 @@
-#include <cstdio>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
 
-int main() {
-  fprintf(stdout, "hello, world\n");
+int main(void) {
+  std::ifstream input("input.txt");
+  std::string sbuf;
+  std::vector<std::string> v;
+  while (getline(input, sbuf)) {
+    v.push_back(sbuf);
+  }
+
+  int incr = 0;
+  int prev_depth = INT32_MAX;
+  for (unsigned long i = 0; i < v.size(); ++i) {
+    int depth = std::stoi(v[i]);
+    if (depth > prev_depth) {
+      ++incr;
+    }
+    prev_depth = depth;
+  }
+  std::cout << incr << std::endl;
 }
 
