7df9dbe0430b33e894ded502bab119c7f49fa5cd
Rob Walker <robertsrwalker@gmail.com>
2021-12-01 00:42:33 -0500

    Add part2

--- 2021/day1/day1.cc
+++ 2021/day1/day1.cc
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 
+const int window_size = 3;
+
 int main(void) {
   std::ifstream input("input.txt");
   std::string sbuf;
@@ -20,6 +22,21 @@ int main(void) {
     }
     prev_depth = depth;
   }
-  std::cout << incr << std::endl;
+  std::cout << "Part 1: " << incr << std::endl;
+
+  int incr_p2 = 0;
+  int prev_sum = INT32_MAX;
+  for (unsigned long i = 0; i < v.size() - window_size + 1; ++i) {
+    int sum = 0;
+    for (unsigned long j = i; j < i + window_size; ++j) {
+      sum += std::stoi(v[j]);
+    }
+
+    if (sum > prev_sum) {
+      ++incr_p2;
+    }
+    prev_sum = sum;
+  }
+  std::cout << "Part 2: " << incr_p2 << std::endl;
 }
 
