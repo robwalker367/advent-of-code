af57b8f3aa66aa616dc48253abf17e9729649c47
Rob Walker <robertsrwalker@gmail.com>
2021-12-01 12:58:52 -0500

    Refactor

--- 2021/day1/day1.cc
+++ 2021/day1/day1.cc
@@ -1,33 +1,25 @@
 #include <vector>
 #include <string>
-#include <fstream>
 #include <iostream>
 
 const int window_size = 3;
 
 int main(void) {
-  std::ifstream input("input.txt");
   std::string s;
   std::vector<int> v;
-  while (getline(input, s)) {
+  while (std::cin >> s) {
     v.push_back(std::stoi(s));
   }
 
   int p1 = 0;
-  int prev_depth = INT32_MAX;
-  for (unsigned long i = 0, n = v.size(); i < n; ++i) {
-    if (v[i] > prev_depth) {
-      ++p1;
-    }
-    prev_depth = v[i];
+  for (unsigned i = 0, n = v.size() - 1; i < n; ++i) {
+    p1 += v[i] < v[i + 1];
   }
   std::cout << "Part 1: " << p1 << std::endl;
 
   int p2 = 0;
-  for (unsigned long i = 0, n = v.size() - window_size; i < n; ++i) {
-    if (v[i] < v[i + window_size]) {
-      ++p2;
-    }
+  for (unsigned i = 0, n = v.size() - window_size; i < n; ++i) {
+    p2 += v[i] < v[i + window_size];
   }
   std::cout << "Part 2: " << p2 << std::endl;
 }
