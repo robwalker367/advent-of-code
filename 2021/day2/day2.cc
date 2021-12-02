29ab0af7ec015c27fdc697796506d55dcd2db1c4
Rob Walker <robertsrwalker@gmail.com>
2021-12-02 14:24:09 -0500

    Add part1

--- 2021/day2/day2.cc
+++ 2021/day2/day2.cc
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+struct command {
+  std::string direction;
+  int weight;
+};
 
 int main() {
-  std::cout << "hello, world" << std::endl;
+  std::vector<command*> commands;
+  std::string s;
+  int w;
+  while (std::cin >> s >> w) {
+    command* c = new command;
+    c->direction = s;
+    c->weight = w;
+    commands.push_back(c);
+  }
+
+  int x = 0, depth = 0;
+  for (unsigned i = 0, n = commands.size(); i < n; ++i) {
+    command* c = commands[i];
+    if (c->direction == "forward") {
+      x += c->weight;
+    } else if (c->direction == "down") {
+      depth += c->weight;
+    } else {
+      depth -= c->weight;
+    }
+  }
+  std::cout << "Part 1: " << x * depth << std::endl;
+
+  for (unsigned i = 0, n = commands.size(); i < n; ++i) {
+    delete commands[i];
+  }
 }
 
