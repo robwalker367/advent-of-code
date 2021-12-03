6d40329f34c9c3e6ed20a5b395e96e04afc311a4
Rob Walker <robertsrwalker@gmail.com>
2021-12-03 11:33:35 -0500

    Add part1

--- 2021/day3/day3.cc
+++ 2021/day3/day3.cc
@@ -1,6 +1,31 @@
+#include <vector>
 #include <iostream>
 
+#define LENGTH 12
+
 int main() {
-  std::cout << "hello, world" << std::endl;
+  long frequencies[LENGTH] = { 0 };
+  long num;
+  int count = 0;
+  while (std::cin >> num) {
+    for (unsigned i = 0; i < LENGTH; ++i) {
+      frequencies[i] += num % 10;
+      num /= 10;
+    }
+    ++count;
+  }
+
+  long gamma = 0, epsilon = 0;
+  for (unsigned i = 0; i < LENGTH; ++i) {
+    gamma <<= 1;
+    epsilon <<= 1;
+    if (count - frequencies[LENGTH - i - 1] > count / 2) {
+      ++gamma;
+    } else {
+      ++epsilon;
+    }
+  }
+
+  std::cout << "Part 1: " << gamma * epsilon << std::endl;
 }
 
