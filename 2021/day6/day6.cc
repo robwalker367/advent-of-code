3c8d36000799809dcf51100dd7c1d8b0b467cecc
Rob Walker <robertsrwalker@gmail.com>
2021-12-20 11:19:37 -0500

    Add day6 part2

--- 2021/day6/day6.cc
+++ 2021/day6/day6.cc
@@ -4,21 +4,28 @@
 
 
 // Number of cycles to breed
-const int cycles = 80;
+const int p1_cycles = 80;
+const int p2_cycles = 256;
+
+
+// Total cycle length for a fish
+
+const long long total_cycle = 9;
 
 
 // Time for a fish to spawn on its first cycle
-const int fst_spawn_cycle = 8;
+const long long fst_spawn_cycle = 8;
 
 
 // Time for a fish to spawn on non-first cycle
-const int std_spawn_cycle = 6;
+const long long std_spawn_cycle = 6;
 
 
 // Helper function prototypes
 
-std::vector<int> read_fish(std::string s);
-void breed_fish(std::vector<int>* fish);
+long long* read_fish(std::string s);
+void breed_fish(long long* fish);
+long long sum_fish(long long* fish);
 
 
 // main()
@@ -28,47 +35,73 @@ int main() {
   // Parse input
   std::string s;
   std::cin >> s;
-  std::vector<int> fish = read_fish(s);
+  long long* fish = read_fish(s);
 
-  // Breed fish
-  for (unsigned i = 0; i < cycles; ++i) {
-    breed_fish(&fish);
+  // Breed fish for p1
+  for (unsigned i = 0; i < p1_cycles; ++i) {
+    breed_fish(fish);
   }
-  std::cout << "P1: " << fish.size() << std::endl;
+  std::cout << "P1: " << sum_fish(fish) << std::endl;
+
+  // Breed fish for p2
+  for (unsigned i = p1_cycles; i < p2_cycles; ++i) {
+    breed_fish(fish);
+  }
+  std::cout << "P2: " << sum_fish(fish) << std::endl;
+
+  // Free memory
+  delete fish;
 }
 
 
 // breed_fish(fish)
 //   Breed fish in `fish`
 
-void breed_fish(std::vector<int>* fish) {
-  unsigned n = fish->size();
-  for (unsigned i = 0; i < n; ++i) {
-    if (--(*fish)[i] < 0) {
-      (*fish)[i] = std_spawn_cycle;
-      fish->push_back(fst_spawn_cycle);
+void breed_fish(long long* fish) {
+  long long new_fish[total_cycle];
+  memset(new_fish, 0, total_cycle * sizeof(long long));
+  for (long long i = 0; i < total_cycle; ++i) {
+    if (i == 0) {
+      new_fish[fst_spawn_cycle] += fish[i];
+      new_fish[std_spawn_cycle] += fish[i];
+    } else {
+      new_fish[i - 1] += fish[i];
     }
   }
+  memcpy(fish, new_fish, total_cycle * sizeof(long long));
+}
+
+
+// sum_fish(fish)
+//   Sum fish in `fish`
+
+long long sum_fish(long long* fish) {
+  long long sum = 0;
+  for (unsigned i = 0; i < total_cycle; ++i) {
+    sum += fish[i];
+  }
+  return sum;
 }
 
 
 // read_fish(s)
 //   Read fish from string `s`
 
-std::vector<int> read_fish(std::string s) {
-  std::vector<int> fish;
+long long* read_fish(std::string s) {
+  long long* fish = new long long[total_cycle];
+  memset(fish, 0, total_cycle * sizeof(long long));
   int num = 0;
   for (unsigned i = 0, n = s.size(); i < n; ++i) {
     std::string c = s.substr(i, 1);
     if (c == ",") {
-      fish.push_back(num);
+      ++fish[num];
       num = 0;
     } else {
       num = num * 10 + std::atoi(c.c_str());
     }
   }
   if (s.substr(s.size() - 1, 1) != ",") {
-    fish.push_back(num);
+    ++fish[num];
   }
   return fish;
 }
