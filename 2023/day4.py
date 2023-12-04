138d04540cddb6e388121a758a1b9367a14432e8
Rob Walker <robertsrwalker@gmail.com>
2023-12-03 21:32:25 -0800

    Add day4 p2

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,12 +1,23 @@
 import helpers
 
-contents = helpers.read_input()
-cards = contents.split('\n')
+input = helpers.read_input().split('\n')
+
+cards = []
+for line in input:
+    cards.append([set([int(x) for x in elt.split()]) for elt in line.split(': ')[1].split(' | ')])
+
 points = 0
-for card in cards:
-    win_nums, nums = [set([int(x) for x in elt.split()]) for elt in card.split(': ')[1].split(' | ')]
+copies = [1] * len(cards)
+for i in range(len(cards)):
+    win_nums, nums = cards[i]
     win_count = len(win_nums.intersection(nums))
+    for j in range(i+1, i+1+win_count):
+        copies[j] += copies[i]
     if win_count > 0:
         points += 2 ** (win_count-1)
+
+# part 1 answer
 print(points)
 
+# part 2 answer
+print(sum(copies))
