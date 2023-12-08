abaaff33819ea98723eaea5f1c50f42a209fed74
Rob Walker <robertsrwalker@gmail.com>
2023-12-07 23:50:45 -0800

    Add day6 p2

--- 2023/day6.py
+++ 2023/day6.py
@@ -14,5 +14,20 @@ races = [
 def get_ways_to_win(race):
     return sum([(race[0] - hold) * hold > race[1] for hold in range(race[0])])
 
+def quad_formula(a, b, c):
+    s1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    s2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    return (s1, s2)
+
 # Part 1
 print(math.prod([get_ways_to_win(race) for race in races]))
+
+# Part 2
+t, d = '', ''
+for race in races:
+    t += str(race[0])
+    d += str(race[1])
+t, d = int(t), int(d)
+
+s1, s2 = quad_formula(-1, t, -d)
+print(math.floor(s2) - math.ceil(s1) + 1)
