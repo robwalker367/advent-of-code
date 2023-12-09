e4402f95fb896f6cd883ee3d9024459497a1b779
Rob Walker <robertsrwalker@gmail.com>
2023-12-08 21:32:40 -0800

    Add day7 p2

--- 2023/day7.py
+++ 2023/day7.py
@@ -72,7 +72,68 @@ def compare(x, y):
                 return idx1 - idx2
     return xval - yval
 
-# Part 1 and 2
+# Part 1
+ranked = sorted(hands, key=cmp_to_key(compare))
+N = len(ranked)
+print(sum([ranked[i][1] * (i+1) for i in range(N)]))
+
+def is_five_of_kind_j(cards):
+    counts = Counter(cards)
+    return is_five_of_kind(cards) or (is_four_of_kind(cards) and counts['J'] in [1, 4]) or (
+        is_full_house(cards) and (counts['J'] in [2,3])
+    )
+
+def is_four_of_kind_j(cards):
+    counts = Counter(cards)
+    return is_four_of_kind(cards) or (is_three_of_kind(cards) and counts['J'] in [1, 3]) or (
+        is_two_pair(cards) and counts['J'] == 2
+    )
+
+def is_full_house_j(cards):
+    counts = Counter(cards)
+    return is_full_house(cards) or (is_three_of_kind(cards) and counts['J'] == 1) or (
+        is_two_pair(cards) and counts['J'] in [1, 2]
+    ) or (is_one_pair(cards) and counts['J'] == 2 and len(counts.values()) < 3) 
+
+def is_three_of_kind_j(cards):
+    return is_three_of_kind(cards) or (is_one_pair(cards) and 'J' in cards)
+
+def is_two_pair_j(cards):
+    return is_two_pair(cards) or (is_one_pair(cards) and 'J' in cards)
+
+def is_one_pair_j(cards):
+    return is_one_pair(cards) or 'J' in cards
+
+strengths = [
+    (is_five_of_kind_j, 7),
+    (is_four_of_kind_j, 6),
+    (is_full_house_j, 5),
+    (is_three_of_kind_j, 4),
+    (is_two_pair_j, 3),
+    (is_one_pair_j, 2),
+    (is_high, 1)
+]
+
+card_values = list(reversed(['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']))
+
+def compare(x, y):
+    xval, yval = None, None
+    for f, w in strengths:
+        if f(x[0]):
+            xval = w
+            break
+    for f, w in strengths:
+        if f(y[0]):
+            yval = w
+            break
+    if xval == yval:
+        for i in range(5):
+            idx1, idx2 = card_values.index(x[0][i]), card_values.index(y[0][i])
+            if idx1 != idx2:
+                return idx1 - idx2
+    return xval - yval
+
+# Part 2
 ranked = sorted(hands, key=cmp_to_key(compare))
 N = len(ranked)
 print(sum([ranked[i][1] * (i+1) for i in range(N)]))
